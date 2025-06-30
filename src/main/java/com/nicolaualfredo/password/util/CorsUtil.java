4101649450a6a1492d40ecce837a7af42ccb6907
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2025-06-30 18:04:23 +0200

    Add CorsUtil to handle CORS headers in HTTP responses

--- src/main/java/com/nicolaualfredo/password/util/CorsUtil.java
+++ src/main/java/com/nicolaualfredo/password/util/CorsUtil.java
@@ -4,10 +4,24 @@
  */
 package com.nicolaualfredo.password.util;
 
+import com.sun.net.httpserver.HttpExchange;
+
 /**
  *
  * @author Nicolau Alfredo
  */
+/**
+ * Utility class to enable CORS for Java HTTP Server responses.
+ */
 public class CorsUtil {
-    
+
+    /**
+     * Adds standard CORS headers to the response. Allows frontend apps (running
+     * on another port or domain) to access the API.
+     */
+    public static void handleCors(HttpExchange exchange) {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+    }
 }
