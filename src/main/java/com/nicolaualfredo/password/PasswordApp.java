260384aae21ddae9ca5513d86b3f8b0b98e4d67b
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2025-06-30 03:54:50 +0200

    🚀 Initial commit: project structure and pom.xml for Password Generator API

---
+++ src/main/java/com/nicolaualfredo/password/PasswordApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nicolaualfredo.password;
+
+/**
+ *
+ * @author Nicolau Alfredo
+ */
+public class PasswordApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
